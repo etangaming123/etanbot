2511505315d0ddefe42fb4e0157dfe7553bc8103
etangaming123 <etangaming123@gmail.com>
2026-05-25 13:25:08 +1000

    file saving with json instead of pickle

--- main.py
+++ main.py
@@ -13,19 +13,29 @@ import re
 intents = discord.Intents.default()
 kokocreditdefaulturl = "https://estore.kokoamusement.com.au/BalanceMobile/BalanceMobile.aspx?i="
 
-datastores = ["linkedkokocards", "profiles"] # pkls to create
+datastores = ["linkedkokocards", "profiles"] # json files to create
 for item in datastores:
-    if not os.path.exists(f"{item}.pkl"):
-        with open(f"{item}.pkl", "wb") as file:
-            pickle.dump({}, file)
-        print(f"Created new file [{item}.pkl]")
+    if os.path.exists(f"{item}.pkl"):
+        data = pickle.load(open(f"{item}.pkl", "rb"))
+        with open(f"{item}.json", "w") as file:
+            json.dump(data, file, indent=4)
+            print(f"Converted [{item}.pkl] to [{item}.json]")
+    if not os.path.exists(f"{item}.json"):
+        with open(f"{item}.json", "w") as file:
+            json.dump({}, file)
+        print(f"Created new file [{item}.json]")
 
-datastoresbuttheseonesarelists = [] # pkls to create but these are lists
+datastoresbuttheseonesarelists = [] # json files to create but these are lists
 for item in datastoresbuttheseonesarelists:
-    if not os.path.exists(f"{item}.pkl"):
-        with open(f"{item}.pkl", "wb") as file:
-            pickle.dump([], file)
-        print(f"Created new file [{item}.pkl]")
+    if os.path.exists(f"{item}.pkl"):
+        data = pickle.load(open(f"{item}.pkl", "rb"))
+        with open(f"{item}.json", "w") as file:
+            json.dump(data, file, indent=4)
+            print(f"Converted [{item}.pkl] to [{item}.json]")
+    if not os.path.exists(f"{item}.json"):
+        with open(f"{item}.json", "w") as file:
+            json.dump([], file)
+        print(f"Created new file [{item}.json]")
 
 def formatUsername(user: discord.User): # Fancy formatting for usernames // displayname (@username)
     if user.display_name == None:
@@ -39,26 +49,26 @@ def getDisplay(user: discord.User): # incase we only want to get display name an
     else:
         return user.display_name
 
-def saveData(store: str, newdata: dict): # Saves data to a specified .pkl file
+def saveData(store: str, newdata: dict): # Saves data to a specified .json file
     print(f"Saving [{store}]...")
     try:
-        backup = loadData(store) 
-        with open(f"{store}_backup.pkl", "wb") as file:
-            pickle.dump(backup, file)
-        with open(f"{store}.pkl", "wb") as file:
-            pickle.dump(newdata, file)
-        os.remove(f"{store}_backup.pkl")
+        backup = loadData(store)
+        with open(f"{store}_backup.json", "w") as file:
+            json.dump(backup, file)
+        with open(f"{store}.json", "w") as file:
+            json.dump(newdata, file)
+        os.remove(f"{store}_backup.json")
         return True # Return true if it succeeds
     except Exception:
         traceback.print_exc()
-        with open(f"{store}.pkl", "wb") as file:
-            pickle.dump(backup, file)
+        with open(f"{store}.json", "w") as file:
+            json.dump(backup, file)
         return False # Otherwise return false
 
-def loadData(store: str): # Gets data from a specified .pkl file
+def loadData(store: str): # Gets data from a specified .json file
     try:
-        with open(f"{store}.pkl", "rb") as file:
-            return pickle.load(file) # Return file data if it succeeds
+        with open(f"{store}.json", "r") as file:
+            return json.load(file) # Return file data if it succeeds
     except Exception:
         traceback.print_exc()
         return "" # Otherwise return an empty string
