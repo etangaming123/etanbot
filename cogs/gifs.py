9b5c8f03b162e2343cd6b9dfa7bfaacbfa10cb5a
etangaming123 <etangaming123@gmail.com>
2026-06-23 22:22:11 +1000

    gif commands

---
+++ cogs/gifs.py
@@ -0,0 +1,66 @@
+import discord  # type: ignore
+from discord import app_commands  # type: ignore
+from discord.ext import commands  # type: ignore
+import traceback
+
+from common import loadData, saveData, formatUsername
+
+config = loadData("config")
+
+class Gifs(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @app_commands.command(name="z-admin-gif-add", description="Adds a gif to the collection!")
+    @app_commands.describe(name="The name of the gif you want to add.", gif_url="The url of the gif you want to add.")
+    async def add_gif(self, interaction: discord.Interaction, name: str, gif_url: str):
+        await interaction.response.defer(ephemeral=True)
+        if not interaction.user.id == config["poweruserid"]:
+            await interaction.edit_original_response(content=f"You don't have permission to use this command.")
+            return
+        gifs = loadData("gifs")
+        if name in gifs.keys():
+            await interaction.edit_original_response(content=f"A gif with that name already exists!")
+            return
+        gifs[name] = gif_url
+        if saveData("gifs", gifs):
+            await interaction.edit_original_response(content=f"Gif added successfully!")
+        else:
+            await interaction.edit_original_response(content=f"An error occurred while adding your gif. Please try again later.")
+
+    @app_commands.command(name="z-admin-gif-remove", description="Removes a gif from the collection.")
+    @app_commands.describe(name="The name of the gif you want to remove.")
+    async def remove_gif(self, interaction: discord.Interaction, name: str):
+        await interaction.response.defer(ephemeral=True)
+        if not interaction.user.id == config["poweruserid"]:
+            await interaction.edit_original_response(content=f"You don't have permission to use this command.")
+            return
+        gifs = loadData("gifs")
+        if name not in gifs.keys():
+            await interaction.edit_original_response(content=f"No gif found with that name!")
+            return
+        del gifs[name]
+        if saveData("gifs", gifs):
+            await interaction.edit_original_response(content=f"Gif removed successfully!")
+        else:
+            await interaction.edit_original_response(content=f"An error occurred while removing your gif. Please try again later.")
+
+    @app_commands.command(name="etanbot-gif", description="Send a gif from the shared collection!")
+    @app_commands.describe(name="The name of the gif you want to view.")
+    async def view_gif(self, interaction: discord.Interaction, name: str):
+        await interaction.response.defer()
+        gifs = loadData("gifs")
+        if name not in gifs.keys():
+            await interaction.edit_original_response(content=f"No gif found with that name!")
+            return
+        await interaction.edit_original_response(content=gifs[name])
+    
+    @view_gif.autocomplete("name")
+    async def view_gif_autocomplete(self, interaction: discord.Interaction, current: str):
+        gifs = loadData("gifs")
+        if len(gifs.keys()) == 0:
+            return ["No gifs found!"]
+        return [app_commands.Choice(name=key, value=key) for key in gifs.keys() if current.lower() in key.lower()][:25]
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(Gifs(bot))
